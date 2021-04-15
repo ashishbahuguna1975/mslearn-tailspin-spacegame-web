b6ff72757171e16c6ef8651dec35c13aed14d4e1
Ashish Bahuguna <ashish_bahuguna@hotmail.com>
2021-04-14 23:12:16 -0400

    .

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace mslearn_tailspin_spacegame_web
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
